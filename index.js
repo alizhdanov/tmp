9c71334854093408df8f5da4cb4826026675af26
Ali Zhdanov <ali.zhdanov@oaks-lab.com>
2019-10-08 23:51:13 +0200

    Add code refactor

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
-const argv = require('yargs').argv;
+
+const qs = require('querystring');
 const fetch = require('node-fetch');
 
-const [currency, value] = argv._;
-
-const [from, to] = currency.split('-');
-
-fetch(
-  `https://api.exchangeratesapi.io/latest?base=${from.toUpperCase()}&symbols=${to.toUpperCase()}`
-)
-  .then(res => res.json())
-  .then(res => {
-    const rate = res.rates[to.toUpperCase()];
-    const result = (value * rate).toFixed(2);
-
-    console.log(
-      `${value} ${from.toUpperCase()} is ${result} ${to.toUpperCase()}`
-    );
-  });
+const argv = require('yargs').command(
+  'convert',
+  'convert one currency to another',
+  () => {},
+  convert
+).argv;
 
 // TODO: add colors
+
+function convert(yargs) {
+  const [command, currency, value] = yargs._;
+  const curencies = currency.split('-');
+
+  const from = curencies[0].toUpperCase();
+  const to = curencies[1].toUpperCase();
+
+  const url = 'https://api.exchangeratesapi.io/latest';
+
+  const params = qs.stringify({ base: from, symbols: to });
+
+  fetch(`${url}?${params}`)
+    .then(res => res.json())
+    .then(res => {
+      const rate = res.rates[to];
+      const result = (value * rate).toFixed(2);
+
+      console.log(`${value} ${from} is ${result} ${to}`);
+    });
+}
